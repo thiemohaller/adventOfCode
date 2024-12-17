3d13b59ef7e0538cdb857ac2ed4d9d82e7f4de62
thiemo_mac <thiemo.haller@me.com>
2024-12-17 15:39:35 +0100

    Implement part two of day 1

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{self, Read};
+use std::collections::HashMap;
 
 fn part_one(path: &str) -> io::Result<i32> {
     let mut file = File::open(path)?;
@@ -57,8 +58,26 @@ fn part_two(path: &str) -> io::Result<i32> {
             }
         }
     }
+    
+    let mut occurance_dictionary: HashMap<i32, usize> = HashMap::new();
+    let mut result_vector: Vec<i32> = Vec::new();
+    
+    // todo optimize this
+    for i in col2.iter() {
+        // todo dive a bit deeper on how dereferencing works in rust
+        *occurance_dictionary.entry(*i).or_insert(0) += 1;
+    }
 
-    return Ok(5);
+    for _j in col1.iter() {
+        if let Some(&occurances) = occurance_dictionary.get(_j) {
+            let current_value = _j * occurances as i32;
+            result_vector.push(current_value);
+        }
+    }
+
+    let similarity_score = result_vector.iter().sum();
+    
+    return Ok(similarity_score);
 }
 
 fn main() {
@@ -67,4 +86,11 @@ fn main() {
         Ok(result) => println!("Part One Result: {}", result),
         Err(e) => eprintln!("Error: {}", e),
     }
+
+    match part_two(path) {
+        Ok(result) => println!("Part Two Result: {}", result),
+        Err(e) => eprintln!("Error: {}", e)
+    }
+
+    println!("So long, and thanks for all the fish! 🐬");
 }
